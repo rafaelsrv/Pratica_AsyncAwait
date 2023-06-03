3896fd8dbe92ae0235573612d53fb54f1fc55d70
Rafael <rafael-srv@hotmail.com>
2023-06-02 23:34:31 -0300

    Fetch js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ fetch('https://economia.awesomeapi.com.br/last/USD-BRL').then(response=>{
     return nomes.USDBRL
     
 }).then(dados=>{
-    document.getElementById("span").innerHTML = dados.name
+    document.getElementById("span").innerHTML = dados.name//
     document.getElementById("div").innerHTML = dados.bid//Tipo de moeda
 })
 //
